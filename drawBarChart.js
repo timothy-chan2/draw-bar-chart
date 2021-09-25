5cc1c7b6942d0d891d5c632b34e2f1fbee8f249b
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-09-25 03:46:10 +0000

    Add unique ID for elements to allow creating multiple charts & reorganize JS into subfunctions

--- drawBarChart.js
+++ drawBarChart.js
@@ -1,68 +1,95 @@
-const drawBarChart = (data, options, element) => {
-  $(".xLabel").css("background-color", 'green');
+const addChartTitle = (num, options) => {
+  $("body").append(`<h2 id="title${num}">${options.title}</h2}>`);
 
-  $("body").append(`<h1>${options.title}</h1}>`);
-  $('h1').css("font-size", options.titleFontSize)
+  $(`#title${num}`).css("font-size", options.titleFontSize)
     .css("color", options.titleColor);
+}
 
-  $("body").append(`<${element} id="chartContainer"></${element}>`);
+const addChartContainer = (num, data, options, element) => {
+  $("body").append(`<${element} id="chartContainer${num}"></${element}>`);
 
-  $("#chartContainer").append(`<div id="yAxis"></div>`)
-    .css("display", "grid")
+  $(`#chartContainer${num}`).css("display", "grid")
     .css("align-items", "end")
-    .css("grid-template-columns", `repeat(${data.length + 2}, ${options.width / data.length}px)`);
+    .css("grid-template-columns", `repeat(${data.length + 1}, ${options.width / data.length}px)`)
+    .css("margin-bottom", "50px");
+}
 
-  $("#yAxis").append(`<div id="yAxisLabel">${options.yLabel.substring(0, 5)}</div>`);
+const addChartYAxis = (num, data, options) => {
+  $(`#chartContainer${num}`).append(`<div id="yAxis${num}"></div>`)
 
-  $("#yAxisLabel").css("margin-right", "10px")
-    .css("font-size", "1.4em");
-
-  $("#yAxis").append(`<div id="yAxisValues"></div>`)
-    .css("height", `${options.height + 30}px`)
+  $(`#yAxis${num}`).css("height", `${options.height + 30}px`)
     .css("margin-bottom", "29px")
     .css("border-right", "2px solid")
     .css("display", "grid")
     .css("justify-items", "end")
     .css("align-items", "end");
 
-  $("#yAxisValues").css("height", `${options.height}px`)
+  $(`#yAxis${num}`).append(`<div id="yAxisLabel${num}">${options.yLabel.substring(0, 5)}</div>`);
+
+  $(`#yAxisLabel${num}`).css("margin-right", "10px")
+    .css("font-size", "1.4em")
+    .css("color", `${options.labelColor}`);
+
+  $(`#yAxis${num}`).append(`<div id="yAxisValues${num}"></div>`);
+
+  $(`#yAxisValues${num}`).css("height", `${options.height}px`)
     .css("display", "grid")
+    .css("align-items", "end");
+
+  $(`#yAxisValues${num}`).append(`<div id="tick${num}4">${(Math.max(...data) * 3 / 4).toFixed(2)} _</div>`)
+    .append(`<div id="tick${num}3">${(Math.max(...data) / 2).toFixed(2)} _</div>`)
+    .append(`<div id="tick${num}2">${(Math.max(...data) / 4).toFixed(2)} _</div>`)
+    .append(`<div id="tick${num}1">0.00 _</div>`);
+}
+
+const addBarContainer = (num, i) => {
+  $(`#chartContainer${num}`).append(`<div id="barContainer${num}${i}"></div>`);
+
+  $(`#barContainer${num}${i}`).css("display", "grid")
     .css("align-items", "end")
-    .append(`<div id="tick1">${(Math.max(...data) * 3 / 4).toFixed(2)} _</div>`)
-    .append(`<div id="tick1">${(Math.max(...data) / 2).toFixed(2)} _</div>`)
-    .append(`<div id="tick1">${(Math.max(...data) / 4).toFixed(2)} _</div>`)
-    .append(`<div id="tick1">0.00 _</div>`);
+    .css("justify-items", "center");
+}
+
+const addBar = (num, i, data, options) => {
+  $(`#barContainer${num}${i}`).append(`<div id="bar${num}${i}"></div>`);
+
+  $(`#bar${num}${i}`).css("height", `${data[Number(i)] * (options.height / Math.max(...data))}px`)
+    .css("width", `${(options.width / data.length) - options.barSpace}px`)
+    .css("display", "grid")
+    .css("align-items", options.valuePos) // "start", "center" or "end" to place position of values in the bar
+    .text(data[Number(i)]);
+
+  $(`#bar${num}${i}`).addClass("bar");
+}
+
+const addChartXAxis = (num, i, data, options) => {
+  $(`#barContainer${num}${i}`).append(`<div id="xAxis${num}${i}"></div>`);
+
+  $(`#xAxis${num}${i}`).css("display", "block")
+    .css("width", `${options.width / data.length}px`)
+    .css("border", "thin solid");
+
+  $(`#barContainer${num}${i}`).append(`<div id="xLab${num}${i}">Bar ${Number(i) + 1}</div>`);
+
+  $(`#xLab${num}${i}`).css("margin-top", "10px")
+    .css("color", options.labelColor);
+
+  $(`#xLab${num}${i}`).addClass("xLabel");
+}
+
+const drawBarChart = (data, options, element, num) => {
+  addChartTitle(num, options);
+  addChartContainer(num, data, options, element);
+  addChartYAxis(num, data, options, element);
 
   for (let i in data) {
-    $("#chartContainer").append(`<div id="barContainer${i}"></div>`);
-
-    $(`#barContainer${i}`).append(`<div id="bar${i}"></div>`)
-      .css("display", "grid")
-      .css("align-items", "end")
-      .css("justify-items", "center");
-
-    $(`#bar${i}`).addClass("bar")
-      .css("height", `${data[Number(i)] * (options.height / Math.max(...data))}px`)
-      .css("width", `${(options.width / data.length) - options.barSpace}px`)
-      .css("display", "grid")
-      .css("align-items", options.valuePos) // "start", "center" or "end" to place position of values in the bar
-      .text(data[Number(i)]);
-
-    $(`#barContainer${i}`).append(`<div id="xAxis${i}"></div>`)
-    $(`#xAxis${i}`).css("display", "block")
-      .css("width", `${options.width / data.length}px`)
-      .css("border", "thin solid");
-
-    $(`#barContainer${i}`).append(`<div id="xLab${i}">Bar ${Number(i) + 1}</div>`);
-    $(`#xLab${i}`).addClass("xLabel")
-      .css("margin-top", "10px");
+    addBarContainer(num, i);
+    addBar(num, i, data, options);
+    addChartXAxis(num, i, data, options);
   }
 
-  $(".bar").css("background-color", options.barColor);
-  $(".xLabel").css("color", options.labelColor);
-  $("#yAxisLabel").css("color", options.labelColor);
-
   for (let i in data) {
-    $(`#xLab${i}`).text(options.xLabels[i]);
+    $(`#bar${num}${i}`).css("background-color", options.barColor);
+    $(`#xLab${num}${i}`).text(options.xLabels[i]);
   }
 }
